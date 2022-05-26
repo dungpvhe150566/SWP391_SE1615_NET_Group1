ab76f99abf79ac56aa163b2fbfe408905cc70ac5
sanglthe150043 <sanglthe150043@fpt.edu.vn>
2022-05-26 13:42:09 +0700

    Update entity3

--- src/java/entity/Manufacturer.java
+++ src/java/entity/Manufacturer.java
@@ -22,4 +22,11 @@ public class Manufacturer {
         this.ManufacturerName = ManufacturerName;
     }
 
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
 }
