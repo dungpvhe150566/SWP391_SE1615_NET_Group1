e7fe1cb1b0dbd16ef59df484f1886d5128994576
PhamVanTrongofa <Trongpvhe151489@fpt.edu.vn>
2022-06-02 18:58:02 +0700

    Update Cart.java

--- src/java/entity/Cart.java
+++ src/java/entity/Cart.java
@@ -20,32 +20,5 @@ public class Cart {
     
     private Product product;
 
-    public int getUserID() {
-        return UserID;
-    }
-
-    public int getProductID() {
-        return ProductID;
-    }
-
-    public int getAmount() {
-        return Amount;
-    }
-
-    public void setUserID(int UserID) {
-        this.UserID = UserID;
-    }
-
-    public void setProductID(int ProductID) {
-        this.ProductID = ProductID;
-    }
-
-    public void setAmount(int Amount) {
-        this.Amount = Amount;
-    }
-
-    @Override
-    public String toString() {
-        return "UserID: "+UserID+"  ProductID: "+ProductID+"  Amount: "+Amount;
-    }
+    
 }
