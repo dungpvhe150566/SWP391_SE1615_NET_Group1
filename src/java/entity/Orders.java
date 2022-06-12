754951a883b775b0213f4a61a09e55c8186b836c
PhamVanTrongofa <Trongpvhe151489@fpt.edu.vn>
2022-06-12 23:11:07 +0700

    Update Orders.java

--- src/java/entity/Orders.java
+++ src/java/entity/Orders.java
@@ -1,5 +1,14 @@
 package entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+
+@Builder
+@Getter
+@Setter
+@ToString
 public class Orders {
 
     private int ID;
@@ -8,58 +17,58 @@ public class Orders {
 
     private int TotalPrice;
 
-    private char Note;
+    private String Note;
 
     private int Status;
 
-    private char DayBuy;
+    private String DayBuy;
 
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public int getUserID() {
-        return UserID;
-    }
-
-    public void setUserID(int UserID) {
-        this.UserID = UserID;
-    }
-
-    public int getTotalPrice() {
-        return TotalPrice;
-    }
-
-    public void setTotalPrice(int TotalPrice) {
-        this.TotalPrice = TotalPrice;
-    }
-
-    public char getNote() {
-        return Note;
-    }
-
-    public void setNote(char Note) {
-        this.Note = Note;
-    }
-
-    public int getStatus() {
-        return Status;
-    }
-
-    public void setStatus(int Status) {
-        this.Status = Status;
-    }
-
-    public char getDayBuy() {
-        return DayBuy;
-    }
-
-    public void setDayBuy(char DayBuy) {
-        this.DayBuy = DayBuy;
-    }
+//    public int getID() {
+//        return ID;
+//    }
+//
+//    public void setID(int ID) {
+//        this.ID = ID;
+//    }
+//
+//    public int getUserID() {
+//        return UserID;
+//    }
+//
+//    public void setUserID(int UserID) {
+//        this.UserID = UserID;
+//    }
+//
+//    public int getTotalPrice() {
+//        return TotalPrice;
+//    }
+//
+//    public void setTotalPrice(int TotalPrice) {
+//        this.TotalPrice = TotalPrice;
+//    }
+//
+//    public char getNote() {
+//        return Note;
+//    }
+//
+//    public void setNote(char Note) {
+//        this.Note = Note;
+//    }
+//
+//    public int getStatus() {
+//        return Status;
+//    }
+//
+//    public void setStatus(int Status) {
+//        this.Status = Status;
+//    }
+//
+//    public char getDayBuy() {
+//        return DayBuy;
+//    }
+//
+//    public void setDayBuy(char DayBuy) {
+//        this.DayBuy = DayBuy;
+//    }
 
 }
